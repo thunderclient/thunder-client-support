27afc5012a1c0ced7d68c9b03ea1d0ba775396af
Ranga Vadhineni <ranga.vadhineni@localmint.com>
2023-07-31 09:09:19 +0100

    updated node modules supported

--- docs/custom-filters.js
+++ docs/custom-filters.js
@@ -1,4 +1,4 @@
-/// version 1.2.2
+/// version 1.2.3
 /// copy tc-types.d.ts file for vscode autocompletion on tc object
 /// <reference path="./tc-types.d.ts" />
 
@@ -6,8 +6,8 @@
 const CryptoJS = require("crypto-js");
 const { v4: uuid4 } = require("uuid");
 const axios = require('axios');  // -- you can use async/await to make requests
-// ---- more node modules supported
-// tough-cookie, ajv, ajv-formats, papaparse, http, https, assert, buffer, stream, url
+// ---- All built-in node modules supported
+// crypto-js, axios, tough-cookie, ajv, ajv-formats, papaparse, http, https, assert, buffer, fs, stream, url
 
 // ---- To load any additional node modules from npm
 // await tc.loadModule("moduleName");  see example below.
