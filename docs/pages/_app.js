d73e3f896572e7847cd5bd0825fd378ea496a0d6
Ranga Vadhineni <rangav@thunderclient.com>
2024-04-06 14:27:44 +0100

    updated docs

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -4,7 +4,7 @@ import Script from "next/script";
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
     return <>
-        <Script strategy="lazyOnload" src="https://eu.umami.is/script.js" data-website-id="78e59035-cbd2-4fa4-891e-3e28259fb02a" />
+        <Script src="https://eu.umami.is/script.js" data-website-id="78e59035-cbd2-4fa4-891e-3e28259fb02a" />
         <Component {...pageProps} />
     </>
 }
